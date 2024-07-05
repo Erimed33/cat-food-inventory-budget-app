906d1f1d973e6b800162af6ca1af7dd55af77ba4
Erimed33 <erikamedina@pursuit.org>
2024-07-05 15:15:48 -0400

    new-project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+import NavBar from './NavBar'
+import Home from './components/Home'
+import { Routes, Route } from 'react-router-dom'
+
+
+function App() {
+ 
+
+  return (
+    <div>
+    <NavBar />
+    <Routes>
+      <Route path='/catfood' element={ <Home />} />
+    </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const Home = () => {
+    const [logs, setLogs] = useState([])
+    useEffect(() => {
+    
+      fetch('https://localhost:3434/catfood')
+          .then(res => res.json())
+          .then(res => {
+              console.log(res)
+          })
+          .catch(err => console.error(err))
+  }, [])
+    
+  return (
+    <div>
+        Home Page
+        </div>
+  )
+}
+
+export default Home
